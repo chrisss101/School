6e10b4a92214529177988dc109b616e4521ead71
chrisss101 <43242832+chrisss101@users.noreply.github.com>
2018-11-04 14:43:14 -0500

    Update Runner.java

--- src/Runner.java
+++ src/Runner.java
@@ -5,24 +5,28 @@ public class Runner {
     public static void turnLeft() {
         int decision = booleanToInt((Math.random() * 1 + 2));
         if (decision == 1) {
-             
+             changeRoom("Math");
         }
          else if (decision == 2) {
-             
+             changeRoom("English");
         }
     }
      public static void turnRight() {
-        int decision = (Math.random() * 1 + 2).toInteger;
+        int decision = (Math.random() * 1 + 2);
         if (decision == 1) {
-            
+            changeRoom("History");
         }
          else if (decision == 2) {
-             
+             changeRoom("Science");
         }
     }
     public static int booleanToInt(boolean value) {
         // Convert true to 1 and false to 0.
         return value ? 1 : 0;
     }
+    Scanner ask = new Scanner(System.in); 
+    System.out.println("What is your name?");
+    String name = ask.nextLine();
+    System.out.println("Heallo " + name);
 
 }
